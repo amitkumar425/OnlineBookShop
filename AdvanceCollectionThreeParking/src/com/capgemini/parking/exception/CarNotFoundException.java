def9c40a1c889bc4a2613eed88f144791c181adc
Kumar <amit.ax.kumar@capgemini.com>
2018-08-06 11:15:14 +0530

    commited on 06/08/2018

--- AdvanceCollectionThreeParking/src/com/capgemini/parking/exception/CarNotFoundException.java
+++
@@ -1,9 +0,0 @@
-//Exception class to handle the situation of illegal car access.
-package com.capgemini.parking.exception;
-
-public class CarNotFoundException extends Exception {
-
-	public CarNotFoundException(String exception) {
-		super(exception);
-	}
-}
