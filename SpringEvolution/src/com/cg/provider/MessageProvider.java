89f722a824371ae0cecbd875b7e4adec7ab04856
Kumar <amit.ax.kumar@capgemini.com>
2018-08-07 09:29:10 +0530

    commited on 07/08/2018

--- SpringEvolution/src/com/cg/provider/MessageProvider.java
+++
@@ -1,7 +0,0 @@
-package com.cg.provider;
-
-public interface MessageProvider {
-
-	public String getMessage();
-
-}
--- SpringEvolution/src/com/cg/renderer/MessageRederer.java
+++
@@ -1,8 +0,0 @@
-package com.cg.renderer;
-
-import com.cg.provider.MessageProvider;
-
-public interface MessageRederer {
-	public void render();
-	public void setMessageProvider(MessageProvider provider);
-}
